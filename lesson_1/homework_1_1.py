d0fcd9ca603b58c4204a0d3da093ad8f851be2cc
taskslayersup <task.slayer@yandex.ru>
2025-08-29 08:15:40 +0300

    done

---
+++ lesson_1/homework_1_1.py
@@ -0,0 +1,52 @@
+# 1. Создание переменных и вывод значений
+name = 'Ruslan'
+age = 30
+height = 1.78
+print('Моё имя:', name, ', мой возраст:', age, ', мой рост:', height)
+# 2. Изменение значений переменных
+# x = 10
+# print(type(x))
+# x = 25.5
+# print(type(x))
+# x = 'Python'
+# print(type(x))
+# print(x)
+# 3. Копирование ссылок
+# a = 7
+# b = a
+# print(a, b)
+# a = 10
+# print(b)
+# 4. Каскадное присваивание
+# x = y = z = 10
+# print(x, y, z)
+# print(id(x))
+# print(id(y))
+# print(id(z))
+# x, y, z = 5, 10, 15
+# print(x, y, z)
+# print(id(x))
+# print(id(y))
+# print(id(z))
+# 5. Обмен значений переменных
+# a = 5
+# b = 10
+# print(a, b)
+# a, b = b, a
+# print(a, b)
+# 6. Работа с именами переменных
+# import keyword
+# print(keyword.kwlist)
+# 7. Использование функции type()
+# var1 = 42
+# va2 = 3.14
+# var3 = 'Hello'
+# print(type(var1), type(va2), type(var3))
+# var1 = 'sorok dva'
+# print(type(var1))
+# 8. Дополнительные задания
+# per1 = 'првт'
+# per2 = 5
+# per3 = 5.5
+# переменная = 10
+# print(переменная)
